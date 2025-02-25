f7bb6b348eaf989701f47593a92ecc5861fa2175
Chander Prakash <cprakash@gmx.de>
2025-02-25 22:07:42 +0100

    trying to fix hex digit validation

--- src/Helper.cpp
+++ src/Helper.cpp
@@ -29,6 +29,54 @@ bool Helper::CheckIfBaseIsValid(const std::string &base) {
 }
 
 bool Helper::CheckIfAddressInRange(const std::string &add) {
+  std::cout << "Helper::CheckIfAddressInRange " << std::endl;
+  int base = 10;
+  if (add[add.length() - 1] == 'h' || add[add.length() - 1] == 'H') {
+    std::cout << "Helper::CheckIfAddressInRange parsing address Numner is "
+                 "invalid hex please check the number "
+              << add << std::endl;
+    for (unsigned long i = 0; i < add.length() - 1; i++) {
+      std::cout << "Helper::CheckIfAddressInRange:: add[i]=" << add[i]
+                << std::endl;
+      if (isdigit(add[i]) && (add[i] < '0' || add[i] > '9')) {
+        std::cout << "Helper::CheckIfAddressInRange Numner is invalid hex "
+                     "isdigit(add[i]) && (add[i] < '0' || add[i] > '9') please "
+                     "check the number "
+                  << add << std::endl;
+        return false;
+      }
+      if ((add[add.length() - 1] == 'H') && isalpha(add[i]) &&
+          (add[i] < 'A' || add[i] > 'F')) {
+        std::cout << "Helper::CheckIfAddressInRange Numner is invalid hex "
+                     "isalpha(add[i]) && (add[i] < 'A' || add[i] > 'F' please "
+                     "check the number "
+                  << add << std::endl;
+        return false;
+      }
+      if ((add[add.length() - 1] == 'h') && isalpha(add[i]) &&
+          (add[i] < 'a' || add[i] > 'f')) {
+        std::cout << "Helper::CheckIfAddressInRange Numner is invalid hex "
+                     "isalpha(add[i]) && (add[i] < 'a' || add[i] > 'f' please "
+                     "check the number "
+                  << add << std::endl;
+        return false;
+      }
+    }
+    base = 16;
+  }
+  // check if number is decimal
+  else if (add[add.length() - 1] == 'd' || add[add.length() - 1] == 'D' ||
+           isdigit(add[add.length() - 1])) {
+    base = 10;
+  }
+  /// check if number is octal
+  if (add[add.length() - 1] == 'o' || add[add.length() - 1] == 'Q') {
+    base = 8;
+  }
+  // check if number is binary
+  else if (add[add.length() - 1] == 'b' || add[add.length() - 1] == 'B') {
+    base = 2;
+  }
   if (std::stoi(add, nullptr, 16) < 0xFFFF)
     return true;
   return false;
