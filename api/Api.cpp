99c42bcec23d16c354088dc6f6b6cf8c9f00b42a
Chander Prakash <cprakash@gmx.de>
2025-03-18 23:01:24 +0100

    working for API

--- api/Api.cpp
+++ api/Api.cpp
@@ -1,1 +1,28 @@
 #include "Api.hpp"
+
+#include <iostream>
+#include <vector>
+
+#include "../includes/FileHandler.hpp"
+#include "../includes/Types.hpp"
+
+Api::Api() {}
+
+/***
+ * function responsible for returning the data required by the frontend, this
+ * means the frontend needs to give this function a file path for the .asm file
+ * this function must parse the .asm file and generate these data structures
+ * 1. generate tokens (for debugging)
+ * 2. generate ROM file of the program in case program was compiled
+ * 3. generate all the log messages for the frontend
+ * 4. generate the other control structure
+ */
+void Api::GetDataForGUI(const char *filePath) {
+  FileHandler m_fileHandler;
+  const std::vector<TokenStruct> m_tokens =
+      m_fileHandler.ReturnTokens("./tests/label.asm");
+
+  for (unsigned long i = 0; i < m_tokens.size(); i++)
+    std::cout << "Tokens received i =" << i
+              << " value =" << m_tokens[i].m_tokenValue << std::endl;
+}
